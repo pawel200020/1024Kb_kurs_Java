b1eaebfd28eaae728c26eb9990317d2bb077f15b
pawel200020 <38167430+pawel200020@users.noreply.github.com>
2021-04-26 00:50:28 +0200

    java cloth app added

---
+++ maven_app/src/main/java/entity/Boots.java
@@ -0,0 +1,28 @@
+package entity;
+
+public class Boots extends Product {
+    private Integer size;
+    private boolean isNaturalSkin;
+
+    public Boots(Long id, String productName, Float price, Float weight, String color, Integer productCount, Integer size, boolean isNaturalSkin) {
+        super(id, productName, price, weight, color, productCount);
+        this.size = size;
+        this.isNaturalSkin = isNaturalSkin;
+    }
+
+    public Integer getSize() {
+        return size;
+    }
+
+    public boolean isNaturalSkin() {
+        return isNaturalSkin;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + "Boots{" +
+                "size='" + size + '\'' +
+                ", isNaturalSkin=" + isNaturalSkin +
+                '}';
+    }
+}
---
+++ maven_app/src/main/java/entity/Cloth.java
@@ -0,0 +1,28 @@
+package entity;
+
+public class Cloth extends Product {
+    private String size;
+    private String material;
+
+    public Cloth(Long id, String productName, Float price, Float weight, String color, Integer productCount, String size, String material) {
+        super(id, productName, price, weight, color, productCount);
+        this.size = size;
+        this.material = material;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public String getMaterial() {
+        return material;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + "Cloth{" +
+                "size='" + size + '\'' +
+                ", material='" + material + '\'' +
+                '}';
+    }
+}
